049f289949a2655c3c658ef82e203110b41d0d91
shapon2004 <shapon2004@hotmail.com>
2026-07-09 23:33:48 +0600

    Added Eureka server and eureka client for service discovery and service registry. Added OpenFeign client to communicate with other services.

---
+++ accounts/src/main/java/com/awf/accounts/service/client/LoansFeignClient.java
@@ -0,0 +1,15 @@
+package com.awf.accounts.service.client;
+
+import com.awf.accounts.dto.CardsDto;
+import com.awf.accounts.dto.LoansDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient("loans")
+public interface LoansFeignClient {
+
+    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    ResponseEntity<LoansDto> fetchLoanDetails(@RequestParam  String mobileNumber);
+}
---
+++ accounts/src/main/java/com/awf/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,55 @@
+package com.awf.accounts.service.impl;
+
+import com.awf.accounts.dto.AccountsDto;
+import com.awf.accounts.dto.CardsDto;
+import com.awf.accounts.dto.CustomerDetailsDto;
+import com.awf.accounts.dto.LoansDto;
+import com.awf.accounts.entity.Accounts;
+import com.awf.accounts.entity.Customer;
+import com.awf.accounts.exception.ResourceNotFoundException;
+import com.awf.accounts.mapper.AccountsMapper;
+import com.awf.accounts.mapper.CustomerMapper;
+import com.awf.accounts.repository.AccountsRepository;
+import com.awf.accounts.repository.CustomerRepository;
+import com.awf.accounts.service.ICustomersService;
+import com.awf.accounts.service.client.CardsFeignClient;
+import com.awf.accounts.service.client.LoansFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    /**
+     * @param mobileNumber - Input Mobile Number
+     * @return Customer Details based on a given mobileNumber
+     */
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
+}
