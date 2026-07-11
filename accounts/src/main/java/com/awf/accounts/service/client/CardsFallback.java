783f682b3ece6813593424daff31a09b64cb1841
shapon2004 <shapon2004@hotmail.com>
2026-07-11 21:49:33 +0600

    Introduced resiliency(circuit breaker pattern)

---
+++ accounts/src/main/java/com/awf/accounts/service/client/CardsFallback.java
@@ -0,0 +1,13 @@
+package com.awf.accounts.service.client;
+
+import com.awf.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
--- accounts/src/main/java/com/awf/accounts/service/impl/CustomersServiceImpl.java
+++ accounts/src/main/java/com/awf/accounts/service/impl/CustomersServiceImpl.java
@@ -45,10 +45,15 @@ public class CustomersServiceImpl implements ICustomersService {
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
         ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(correlationId, mobileNumber);
-        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        if(loansDtoResponseEntity!=null){
+            customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        }
+
 
         ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(correlationId, mobileNumber);
-        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        if(cardsDtoResponseEntity!=null) {
+            customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        }
 
         return customerDetailsDto;
 
